92c3bd7cf46651a65e874b55b066cdb37f4f038d
Russell Lilljedahl <russell.lilljedahl@gmail.com>
2018-07-08 13:41:25 -0400

    stash

--- Workout Logger/newentry.php
+++ Workout Logger/newentry.php
@@ -4,17 +4,24 @@
     // replace with user login
     $conn = new mysqli($const['localhost'], $const['rlill'], $const['test1234'],$const['database']) or die("Connect failed: %s\n". $conn -> error);
     // Check connection
-    if($mysqli === false){
+    if($conn === false){
         die("ERROR: Could not connect. " . $mysqli->connect_error);
-    }    
-    
+    } 
+      
     // insert into table
-    $sql = "INSERT INTO workout_table (exercise_name, workout_date, 
-            . reps, weight, sets, notes) 
+    $sql = "INSERT INTO 'workout_table' ('exercise_name', 'workout_date', 
+            . 'reps', 'weight', 'sets', 'notes') 
             VALUES
-            (exerciseName, workoutDate, exerciseReps , 
-            exerciseWeight, exerciseSets, notes)";
-    header("location:dataSubmitted.html"); // if pressed, return to workout log page
+            ([exerciseName], [workoutDate], [exerciseReps], 
+            [exerciseWeight], [exerciseSets], [notes])";
+    
+    if($conn->query($sql) === true) {
+        header("location:dataSubmitted.html"); // if pressed, return to workout log page
+    } else{
+        header("location:dataEntryFailed.html"); // if pressed, return to workout log page
+    }
+    
+    
     
     //TODO
     // add other funcitons as necessary
