4a73badb419da2aff21915dfd58eef08663d4b76
Russell Lilljedahl <russell.lilljedahl@gmail.com>
2018-07-08 14:26:59 -0400

    Added history and finished conforming to style

--- Workout Logger/history.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'session.php'; // cannot get to this page without being logged in
-?>
-
-
--- Workout Logger/newentry.php
+++ Workout Logger/newentry.php
@@ -1,28 +1,23 @@
 <?php
+    session_start();
 
-    $const = include("dbconfig.php");
     // replace with user login
-    $conn = new mysqli($const['localhost'], $const['rlill'], $const['test1234'],$const['database']) or die("Connect failed: %s\n". $conn -> error);
+    $conn = new mysqli("localhost", "rlill", "test1234","cmsc_495") or die("Connect failed: %s\n". $conn -> error);
     // Check connection
     if($conn === false){
         die("ERROR: Could not connect. " . $mysqli->connect_error);
     } 
-      
+    
     // insert into table
-    $sql = "INSERT INTO 'workout_table' ('exercise_name', 'workout_date', 
-            . 'reps', 'weight', 'sets', 'notes') 
-            VALUES
-            ([exerciseName], [workoutDate], [exerciseReps], 
-            [exerciseWeight], [exerciseSets], [notes])";
+    $sql = "INSERT INTO workout_table (exercise_name, workout_date, reps, weight, sets, notes) 
+            VALUES ('exerciseName', 'workoutDate', 'exerciseReps', 'exerciseWeight', 'exerciseSets', 'notes')";
     
     if($conn->query($sql) === true) {
         header("location:dataSubmitted.html"); // if pressed, return to workout log page
     } else{
         header("location:dataEntryFailed.html"); // if pressed, return to workout log page
     }
-    
-    
-    
+        
     //TODO
     // add other funcitons as necessary
     // delete, change, etc.
